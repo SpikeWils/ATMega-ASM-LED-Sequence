69efd052c9aead252166ef27958626bcf6421b6a
Spike Wilson <113636901+SpikeWils@users.noreply.github.com>
2023-05-19 01:28:02 +0100

    Comprehensive Commenting Added
    Added explicit commenting per line

--- main.asm
+++ main.asm
@@ -1,40 +1,40 @@
-ldi r16, HIGH(RAMEND)		;initialise stack pointer
-out SPH, r16
-ldi r16, LOW(RAMEND)
-out SPL, r16
+ldi r16, HIGH(RAMEND)     ; Load immediate "high byte" of RAMEND into r16 - this initializes the stack pointer high byte
+out SPH, r16              ; Output the contents of r16 to Stack Pointer High (SPH)
+ldi r16, LOW(RAMEND)      ; Load immediate "low byte" of RAMEND into r16 - this initializes the stack pointer low byte
+out SPL, r16              ; Output the contents of r16 to Stack Pointer Low (SPL)
 
-ldi r17, 0b00000111
-out DDRB, r17				;set PortB pins 0,1,2 as output
-ldi r18, 0b10000000
-out DDRD, r18				;set PortD pin 7 as input
+ldi r17, 0b00000111       ; Load immediate value '00000111' into r17
+out DDRB, r17             ; Set the first three pins of port B as output (Data Direction Register B)
 
+ldi r18, 0b10000000       ; Load immediate value '10000000' into r18
+out DDRD, r18             ; Set the eighth pin of port D as input (Data Direction Register D)
 
-begin:						;program start
-ldi r19, 0b00000000	
-out PORTB, r19				;set portB to zero
+begin:                     ; Label for the beginning of the program
+ldi r19, 0b00000000
+out PORTB, r19            ; Set all pins of port B to zero
 
+rcall DelayT1             ; Call subroutine DelayT1
+cbi PORTB, 0              ; Clear the bit 0 of Port B
+rcall DelayT1             ; Call subroutine DelayT1 again
+rjmp begin                ; Unconditional jump back to the 'begin' label
 
+DelayT1:                  ; Label for the DelayT1 subroutine
+ldi r16, 0xfd             ; Load immediate value '0xfd' into r16
+sts TCNT1H, r16           ; Store direct from data space (sets Timer/Counter 1 – High Byte to r16 value)
+ldi r16, 0x8f             ; Load immediate value '0x8f' into r16
+sts TCNT1L, r16           ; Store direct from data space (sets Timer/Counter 1 – Low Byte to r16 value)
+ldi r20, 0                ; Load immediate value '0' into r20
+sts TCCR1A, r20           ; Set Timer/Counter1 Control Register A to normal mode
 
-rcall DelayT1
-cbi PORTB, 0
-rcall DelayT1
-rjmp begin
+ldi r20, 0x04             ; Load immediate value '0x04' into r20 for prescaling of /256
+sts TCCR1B, r20           ; Set Timer/Counter1 Control Register B to start timer
 
-DelayT1:
-ldi r16, 0xfd
-sts TCNT1H, r16
-ldi r16, 0x8f
-sts TCNT1L, r16
-ldi r20, 0
-sts TCCR1A, r20				;timer counter 1 normal mode
-ldi r20, 0x04				;prescaling of /256
-sts TCCR1B, r20				;start timer one
+loop:                      ; Label for a loop
+sbis TIFR1, TOV1          ; Skip if bit in I/O register is set (skip next instruction if TOV1 bit in TIFR1 is set)
+rjmp loop                  ; Unconditional jump back to the 'loop' label
 
-loop:
-sbis TIFR1, TOV1
-rjmp loop
-ldi r20, 0
-sts TCCR1A, r20				;stop timer
-ldi r20, 1					;clear flag
-out TIFR1, r20				;reset flag
-ret
+ldi r20, 0                ; Load immediate value '0' into r20
+sts TCCR1A, r20           ; Stop timer by resetting Timer/Counter1 Control Register A
+ldi r20, 1                ; Load immediate value '1' into r20 to clear flag
+out TIFR1, r20            ; Output the content of r20 to Timer/Counter 1 Flag Register (TIFR1), effectively resetting the flag
+ret                        ; Return from subroutine
